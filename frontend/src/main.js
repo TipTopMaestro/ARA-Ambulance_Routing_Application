c2cdac3671e5331de4b96df02a49bbdd955731ce
adeptaixiao24 <maestrotiptop@gmail.com>
2026-05-18 11:25:12 +0800

    chore: stop tracking      node_modules

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './App.css'
-import '@fortawesome/fontawesome-free/css/all.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
 const app = createApp(App)
